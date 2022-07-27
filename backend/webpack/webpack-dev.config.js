b84f230145dc2f97b668c8dbed196e6477e7c0bc
Gepolter <73522230+Gepolter@users.noreply.github.com>
2022-07-27 22:25:44 +0200

    json update6

--- backend/webpack/webpack-dev.config.js
+++ backend/webpack/webpack-dev.config.js
@@ -10,14 +10,14 @@ module.exports = {
   target: 'node',
   plugins: [
     new webpack.DefinePlugin({
-      ENVIRONMENT: JSON.stringify('production'),
+      //ENVIRONMENT: JSON.stringify('production'),
       PORT: JSON.stringify('3000'),
       GREETING_MESSAGE: JSON.stringify('API Running In Development Environment'),
       API_WORKS_MESSAGE: JSON.stringify('API_WORKS_MESSAGE'),
       DB_USERNAME: JSON.stringify('DB_USERNAME'),
       DB_PASSWORD: JSON.stringify('DB_PASSWORD'),
       DB_CONNECTION_STR: JSON.stringify('mongodb+srv://Gepolter:KrummeMhrGoDB239$@mhrgodb.h9kql.mongodb.net/MhrGoTestData?retryWrites=true&w=majority'),
-      'process.env.NODE_ENV': JSON.stringify('production')
+     // 'process.env.NODE_ENV': JSON.stringify('production')
     }),
   ],
   mode:'production',
